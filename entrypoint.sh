fff31d2763e4d5cb85f29731bdc91f72c02be190
Greg Friedholm <139180378+gfriedholm@users.noreply.github.com>
2024-03-22 10:53:35 -0600

    Create entrypoint.sh
    entry point for stm32 build

---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managebuilder.core.headlessbuild -data /tmp/stm-workspace -import "$1"
+headless-build.sh -data /tmp/stm-workspace -build "$2"
